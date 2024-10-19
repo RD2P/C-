9ec2d6bc527eb6b2a5ad6d2182c54d709f9d87ba
Raphael <graphael.dr@gmail.com>
2024-10-18 22:32:39 -0600

    loops

---
+++ loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+  // for(int i = 0; i < 10; i++){
+  //   std::cout << i << std::endl;
+  // }
+
+  // int i = 0;
+  // while (i < 10) {
+  //   std::cout << i << std::endl;
+  //   i++;
+  // }
+
+  // int nums[] = {1, 2, 3, 4};
+  // for (int num: nums){
+  //   std::cout << num << std::endl;
+  // }
+
+  return 0;
+}
