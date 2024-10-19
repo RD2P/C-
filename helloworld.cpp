9b00f50ffbea8f88a5c145e9852b8ea38328008c
Raphael <graphael.dr@gmail.com>
2024-10-18 22:07:42 -0600

    input and output stream

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello world";
+  const int pair = 2;
+  std::cout << pair << "s" << "\n";
+  // std::cout << 2 * 3;
+  // std::cout << "size of pair: " << sizeof(pair);
+
+  std::cout << sizeof(int);
+
+  
+  // int num;
+  // std::cin >> num;
+  // std::cout << num;
   return 0;
 }
