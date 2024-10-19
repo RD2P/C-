1af4f3e3ed61991e4abb3f43782ddcd443506bde
Raphael <graphael.dr@gmail.com>
2024-10-19 10:33:11 -0600

    arrays, vectors

---
+++ sequences.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+  // string names[] = { "Bob", "Joe", "Bill"};
+  // cout << names[0];
+
+  // for (string name: names){
+  //   cout << name << endl;
+  // }
+
+
+  // vector<string> fruits = {"apple", "banana", "cherry"};
+  // fruits.push_back("date");
+  // cout << fruits[3];
+
+  return 0; 
+}
