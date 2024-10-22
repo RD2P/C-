1b9278db4679c14b92f4655b91a382970ad6c682
Raphael <graphael.dr@gmail.com>
2024-10-21 21:29:34 -0600

    sequences

--- sequences.cpp
+++ sequences.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <array>
 
 using namespace std;
 
@@ -17,10 +18,13 @@ int main(){
   // fruits.push_back("date");
   // cout << fruits[3];
 
-  // Getting size of array
+  // Getting size of array using sizeof - not reliable
   // int nums[] = {1,2,3,4,5,6};
   // cout << sizeof(nums) / sizeof(int);
 
-  
+  // Getting size of Array class
+  array<int> nums = {1,2,3,4,5};
+  cout << nums.size();
+
   return 0; 
 }
