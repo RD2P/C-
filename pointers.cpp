0f7cc299386c03bb5f14782100f91b1acc05c31f
Raphael <graphael.dr@gmail.com>
2024-12-13 23:49:51 -0600

    dereferencing pointer, changing value in memory address

--- pointers.cpp
+++ pointers.cpp
@@ -6,11 +6,20 @@ int main(){
   int* pnum = &num;
   int** ppnum = &pnum; // pointer to pointer
   int nval = *pnum; // dereference pointer
+  
 
   std::cout << "num " << num << '\n';
-  std::cout << "pnum" << pnum << '\n';
-  std::cout << "ppnum" << ppnum << '\n';
-  std::cout << "nval " << nval;
+  std::cout << "pnum " << pnum << '\n';
+  std::cout << "ppnum " << ppnum << '\n';
+  std::cout << "nval " << nval << '\n';
+
+  std::cout << &pnum << '\n';
+  *pnum = 77; // change value referenced by the pointer
+  std::cout << "new num " << num << '\n'; // 77
+
+  int* pnval = &nval;
+  *pnval = 99;
+  std::cout << "new nval " << nval;
 
   return 0;
 }
