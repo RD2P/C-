ff3ddefd4fce28c02548504e5aa29781e311d724
Raphael <graphael.dr@gmail.com>
2024-12-13 23:34:11 -0600

    mem address

--- reference.cpp
+++ reference.cpp
@@ -7,12 +7,16 @@ int main(){
   num+=3;
   std::cout << "num : " << num << '\n'; // 11
   std::cout << "numref : " <<  numref << '\n'; // 11
+  std::cout << "&num: " << &num << '\n';
+  std::cout << "&numref: " << &numref << '\n'; // same memory address, points to num
+
 
   int num2 = 7;
   int num3 = num2;
   num3 += 99;
   std::cout << "num 2: " << num2 << '\n'; // 7
   std::cout << "num 3: " << num3 << '\n'; // 101
+
 
   return 0;
 }
